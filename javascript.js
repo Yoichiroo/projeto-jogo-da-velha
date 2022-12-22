7ac8243bd64edac539c488a3830a77f32ef016aa
Lucas Torres <repositoriolucas@gmail.com>
2022-12-22 15:41:59 -0300

    Verificação de quem está jogando e a criação da matriz para começarmos o jogo.

--- javascript.js
+++ javascript.js
@@ -1,2 +1,41 @@
 let jogo = []
 let tabuleiro = []
+let quemJoga = 0 // 0 - JOGADOR = X; 1 - CPU = O
+let verificar //VERIFICAR SE HOUVE VENCEDOR OU NÃO
+let jogando = true
+let nivel = 1
+let jogadaCPU = 1
+let quemComeca = Math.round(Math.random() * 1)
+
+//  JOGANDO
+
+function jogar(pos) {
+    //VERIFICAÇÃO DO JOGADOR QUE ESTÁ JOGANDO 
+    if((jogando) && (quemJoga == 0)) {
+        switch(pos) {
+            case 1:
+                if(jogo[0][0 == ""]) {
+                    jogo[0][0] = "X"
+                    quemJoga = 1
+                } else {
+                    
+                }
+                break
+        }
+    }
+}   
+
+function iniciar() {
+    jogando = true
+    jogadaCPU = 1
+    jogo = [
+        //MATRIZ COM 3 LINHAS E CADA LINHA TEM 3 COLUNAS. SERVIRÃO PARA A NOSSA IA JOGAR.
+
+        ["", "", ""] // 0,0 | 0,1 | 0,2
+        ["", "", ""] // 1,0 | 1,1 | 1,2
+        ["", "", ""] // 2,0 | 2,1 | 2,2
+
+    ]
+}
+
+window.addEventListener("load", iniciar)
